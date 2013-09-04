2ac6346fd92ac6b0a45de87fb1579e7eda10c726
Hannes Verschore <hverschore@mozilla.com>
2013-09-04 12:20:15 +0200

    Small fixes to auto.py

--- auto.py
+++ auto.py
@@ -18,24 +18,20 @@ benchmarks = ['richards',
 'code-load',
 'box2d']
 
-rev = "5efda3c30be3"
+rev = "82d28bdf9317"
 octane = "/home/h4writer/Build/octane"
 
 def bench(bench_name, js_file, html_file):
   global octane, js
 
   print run("cd "+octane+"; "+js+" --ion-parallel-compile=on "+js_file),
-  print run("cd /home/h4writer/Build/tracelogger/output; python ../generate.py /tmp/tracelogging.log 80000 \""+bench_name+"\" > "+html_file),
+  print run("cd /home/h4writer/Build/tracelogger/output; pypy ../generate.py /tmp/tracelogging.log 80000 \""+bench_name+"\" > "+html_file),
 
-js = "/home/h4writer/Build/mozilla-inbound/js/src/build-64/js"
+js = "/home/h4writer/Build/mozilla-inbound/js/src/build-tracelogging/js"
 for i in benchmarks:
   bench("Octane-"+i, "run-"+i+".js", "octane-"+i+"-"+rev+".html")
 
-js = "/home/h4writer/Build/mozilla-inbound/js/src/build-ggc/js"
+js = "/home/h4writer/Build/mozilla-inbound/js/src/build-tracelogging-ggc/js"
 for i in benchmarks:
   bench("Octane-"+i+" GGC", "run-"+i+".js", "octane-"+i+"-ggc-"+rev+".html")
 
-#  run("cd "+octane+"; "+js+" --ion-parallel-compile=on run-"+i+".js")
-#  run("cd /home/h4writer/Build/tracelogger/output; python ../generate.py /tmp/tracelogging.log 20000 Octane-"+i+" "+rev+" > octane-"+i+"-"+rev+".html")
-#  run("cd "+octane+"; "+js+" --ion-parallel-compile=on run-"+i+".js")
-#  run("cd /home/h4writer/Build/tracelogger/output; python ../generate.py /tmp/tracelogging.log 20000 \"Octane-"+i+" GGC\" "+rev+" > octane-"+i+"-ggc-"+rev+".html")
