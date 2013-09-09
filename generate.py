9a7f3034abadb0841b500d05bb053f73328a4995
Hannes Verschore <hverschore@mozilla.com>
2013-09-10 00:53:36 +0200

    Phase 1: Abstract reader to allow multiple readers

--- generate.py
+++ generate.py
@@ -4,26 +4,28 @@ import argparse
 import tailer
 
 argparser = argparse.ArgumentParser(description='Parse tracelogs and create html output for them.')
-argparser.add_argument('logfile',
+argparser.add_argument('logfile', nargs='+',
                    help='the logfile to parse')
-argparser.add_argument('outfile',
+argparser.add_argument('-o', '--outfile', default="stdout",
                    help='the name of the html output file')
-argparser.add_argument('-w', '--width', type=int, default=80000,
-                   help='width in px (default: 80000)')
+argparser.add_argument('-w', '--width', type=int, default=40000,
+                   help='width in px (default: 40000)')
 argparser.add_argument('-n', '--name', default=None,
                    help='Benchmark name')
 argparser.add_argument('-r', '--revision', default=None,
                    help='Revision the shell or browser executable was built from')
 args = argparser.parse_args()
 
-logFilename = args.logfile
+logFilenames = args.logfile
 outFilename = args.outfile
 pixels = args.width
 bench_name = args.name
 revno = args.revision
 
-tail = tailer.tail(open(logFilename), 1)[0].split(",")[0]
-ticks = int(tail)
+ticks = 0
+for logFilename in logFilenames:
+    tail = tailer.tail(open(logFilename), 1)[0].split(",")[0]
+    ticks = max(ticks, int(tail))
 zoom = pixels*1./ticks
 time = 0
 
@@ -31,7 +33,12 @@ start = 0
 status = ""
 oldline = ""
 
-outfile = open(outFilename, 'w')
+if outFilename == "stdout":
+    outfile = sys.stdout
+elif outFilename == "stderr":
+    outfile = sys.stderr
+else:
+    outfile = open(outFilename, 'w')
 
 names = {
   "s i": "interpreter run",
@@ -80,7 +87,7 @@ outfile.write("<!--<div><p>1px = "+str(int(1./zoom))+" kernel ticks</p></div>-->
 outfile.write("</div>\n")
 outfile.write("<div class='graph'>\n")
 
-def create_backtrace():
+def create_backtrace(stack):
     full_info = ""
     for entry in stack:
         full_info += "; -"+",".join(entry["data"][2:])
@@ -90,7 +97,7 @@ def create_backtrace():
 
 block_time = 0
 aggregate = 0
-def output(delta, info):
+def output(delta, info, stack):
     global text, block_time, aggregate
 
     width = delta * zoom + aggregate
@@ -118,7 +125,7 @@ def output(delta, info):
     for i in range(int(width/block_width)):
       blocks += "<span style='width:"+str(block_width)+"px'>\n</span>"
 
-    full_info = create_backtrace()
+    full_info = create_backtrace(stack)
 
     outfile.write("<span class='block "+names[class_]+"' info='Block: "+str(block_time)+";Engine: "+engines[engine]+";<b>Call stack:</b>"+full_info+"'>"+blocks+"</span>")
     block_time += 1
@@ -146,8 +153,9 @@ def keep_stat(delta, info):
       script = data[3]
       script_stat[script][statkey] += delta
 
-def keep_stat_start(data):
+def keep_stat_start(info):
   global next_text_id
+  data = info["data"]
   task = data[2]
   # Any script running / ion compiling / any parsing
   if task == "s" or task == "c" or task[0] == "p":
@@ -165,34 +173,79 @@ def keep_stat_start(data):
 
 ##################################################""
 
-stack = []
+class LogReader:
 
-if ".gz" in logFilename:
-    fp = gzip.open(logFilename)
-else:
-    fp = open(logFilename)
+    def __init__(self, name):
+        self.stack_ = [{"data":["0","1","s"]}]
+        self.time = 0
+        self.current_ = None
+        self.next_ = None
+        self.done_ = False
 
-for line in fp:
-    data = line[:-1].split(",")
-    if len(data) < 2:
-        continue
-    # Any start/stop or engine change.
-    if data[1] == "1" or data[1] == "0" or (data[1] == "e"):
-        time = int(data[0])
-        if data[1] == "1":
-            stack.append({"data":data})
-            keep_stat_start(data)
+        if ".gz" in name:
+            self.fp = gzip.open(name)
         else:
-            output(time-prev_time, stack[-1])
-            keep_stat(time-prev_time, stack[-1])
-            
-            if data[1] == "e":
-                stack[-1]["engine"] = data[2][0]
-            else:
-                assert data[1] == "0"
-                stack = stack[:-1]
+            self.fp = open(name)
 
-    prev_time = time
+    def next(self):
+        while True:
+            try:
+                line = self.fp.next()
+                next_ = line[:-1].split(",")
+                print line,
+            except StopIteration:
+                self.done_ = True
+                return
+            
+            # Only process data with timestamp,event 
+            if len(next_) < 2:
+                continue
+
+            # Only process data for start/stop or engine change.
+            if next_[1] not in ["1","0","e"]:
+                continue
+
+            self.current_ = self.next_
+            self.next_ = next_
+
+            if self.current_ == None:
+                continue
+
+            self.duration = int(self.next_[0]) - int(self.current_[0])
+
+            if self.isStart():
+                self.stack_.append({"data": self.current_})
+            else:
+                if self.isEngineChange():
+                    self.stack_[-1]["engine"] = self.current_[2][0]
+                else:
+                    assert self.isStop()
+                    self.stack_ = self.stack_[:-1]
+            return
+
+    def isStart(self):
+        return self.current_[1] == "1"
+    def isStop(self):
+        return self.current_[1] == "0"
+    def isEngineChange(self):
+        return self.current_[1] == "e"
+    def info(self):
+        return self.stack_[-1]
+    def isDone(self):
+        return self.done_
+    def stack(self):
+        return self.stack_
+
+for logFilename in logFilenames:
+    reader = LogReader(logFilename)
+    while not reader.isDone():
+        reader.next()
+
+        if reader.isStart():
+            keep_stat_start(reader.info())
+
+        output(reader.duration, reader.info(), reader.stack())
+        keep_stat(reader.duration, reader.info())
 
 ##########################################################
 outfile.write("</div>\n")
