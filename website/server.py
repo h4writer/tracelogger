666b4cd6e84dbb5d0f8d951d7adb3f2e69fa1362
Steve Fink <sphink@gmail.com>
2016-08-22 18:22:43 -0700

    Allow running server.py from within directory containing data (or passing datadir on command line)

--- website/server.py
+++ website/server.py
@@ -4,7 +4,7 @@ import BaseHTTPServer
 import os
 import sys
 
-datadir = os.getcwd()
+datadir = None
 
 class TraceLoggerRequestHandler(SimpleHTTPRequestHandler):
     # Add CORS header
@@ -12,23 +12,35 @@ class TraceLoggerRequestHandler(SimpleHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Origin', '*')
         SimpleHTTPRequestHandler.end_headers(self)
 
-    def translate_path(self, path):
+    def translate_path(self, url_path):
         '''Pass through most commands to the default translation, which resolves
-           relative to the current working directory. Directory listings and filenames
-           starting with "tl-" are resolved relative to the data directory.'''
-        cwdpath = SimpleHTTPRequestHandler.translate_path(self, path)
-        if cwdpath is None:
-            return cwdpath
-        if os.path.isdir(cwdpath) or os.path.basename(cwdpath).startswith("tl-"):
+           relative to the current working directory (containing the website/
+           files). Directory listings, *.json, and *.tl files are resolved
+           relative to the data directory.
+        '''
+        path = SimpleHTTPRequestHandler.translate_path(self, url_path)
+        if path is None:
+            return path
+        if os.path.isdir(path) or path.endswith(".json") or path.endswith(".tl"):
             discard = len(os.getcwd()) + 1
-            return os.path.join(datadir, cwdpath[discard:])
-        return cwdpath
+            return os.path.join(datadir, path[discard:])
+        return path
 
 if __name__ == '__main__':
     # Interpret non-numeric first argument as a data directory.
-    if len(sys.argv) > 0:
+    if len(sys.argv) > 1:
         try:
             port = int(sys.argv[1])
         except:
             datadir = sys.argv.pop(1)
+
+    if datadir is None:
+        datadir = os.getcwd()
+
+    # If not running from within the website/ directory, assume we are running
+    # from the data directory. cd to the website dir so we serve non-data files
+    # from there.
+    website_dir = os.path.realpath(os.path.dirname(__file__))
+    os.chdir(website_dir)
+
     BaseHTTPServer.test(TraceLoggerRequestHandler, BaseHTTPServer.HTTPServer)
