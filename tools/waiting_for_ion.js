6f4553c94c33f421576804f4c3c820295adadb0a
Hannes Verschore <hv1989@gmail.com>
2016-10-31 15:10:12 +0100

    improve metrics of waiting_for_ion

--- tools/waiting_for_ion.js
+++ tools/waiting_for_ion.js
@@ -5,12 +5,12 @@ var array = read(data["tree"], 'binary');
 var tree = new DataTree(array.buffer, textmap);
 
 /**
- * This tool computes the time we are waiting for an 
+ * This tool computes the time we are waiting for an
  * IonScript to be ready after deciding to start ion compiling.
- * 
+ *
  * Note: the result is not the time we could have been faster.
  * It is an indication. One assumes IonMonkey executes 10x to 100x
- * times faster than Baseline. As a result the time spend could 
+ * times faster than Baseline. As a result the time spend could
  * (conservative) be executed 10x faster.
  */
 
@@ -33,30 +33,13 @@ function iterate(tree, id) {
 
     /* Iterate the childs of this log item. */
     var childs = tree.childs(id);
+    var child_scripts_time = 0;
     for (var i = 0; i < childs.length; i++) {
-        parent_id = id; 
-        total_time -= tree.stop(childs[i]) - tree.start(childs[i]);
-        iterate(tree, childs[i]);
-    }
-
-    /* Compute total time, exclusive childs time. */
-    var total_time = tree.stop(id) - tree.start(id);
-    for (var i = 0; i < childs.length; i++) {
-        total_time -= tree.stop(childs[i]) - tree.start(childs[i]);
+        parent_id = id;
+        child_scripts_time += iterate(tree, childs[i]);
     }
 
     parent_id = last_parent_id;
-
-    if (tree.text(id) == "Interpreter" || tree.text(id) == "Baseline") {
-        // We always log the script name and afterwards that we started compiling.
-        var scriptname = tree.text(parent_id);
-        if (compiling[scriptname]) {
-            waiting_for_ion += total_time;
-            if (!waited[scriptname])
-                waited[scriptname] = 0;
-            waited[scriptname] += total_time;
-        }
-    }
 
     /* Mark we ended compiling a script. */
     if (tree.text(id) == "IonLinking") {
@@ -64,6 +47,24 @@ function iterate(tree, id) {
         var scriptname = tree.text(parent_id);
         compiling[scriptname] = false;
     }
+
+    /* Save the amount of time we spend executing this exact script if we are currently compiling it. */
+    if (tree.text(id) == "Interpreter" || tree.text(id) == "Baseline") {
+        // We always log the script name and afterwards that we started compiling.
+        var scriptname = tree.text(parent_id);
+        if (compiling[scriptname]) {
+            var total_time = tree.stop(id) - tree.start(id);
+            waiting_for_ion += total_time - child_scripts_time;
+            if (!waited[scriptname])
+                waited[scriptname] = 0;
+            waited[scriptname] += total_time - child_scripts_time;
+        }
+    }
+
+    /* Return the amount we spend in child scripts */
+    if (tree.text(id) == "Interpreter" || tree.text(id) == "Baseline" || tree.text(id) == "IonMonkey")
+        return tree.stop(id) - tree.start(id);
+    return child_scripts_time;
 }
 
 var compiling = {};
@@ -74,11 +75,11 @@ var parent_id = 0;
 iterate(tree, 0);
 
 var total_time = tree.stop(0) - tree.start(0);
-print("Waited for ion: " + waiting_for_ion, "("+waiting_for_ion/total_time+"%)") 
-print("out of execution time: " + total_time) 
+print("Waited for ion: " + waiting_for_ion, "("+waiting_for_ion/total_time*100+"%)")
+print("out of execution time: " + total_time)
 print("--------------------")
 var keys = Object.keys(waited);
 keys.sort(function(a,b) {return waited[b] - waited[a]});
 for (var i = 0; i < keys.length; i++) {
-    print (keys[i] + ": " + waited[keys[i]], "("+waited[keys[i]]/waiting_for_ion+"%)");
+    print (keys[i] + ": " + waited[keys[i]], "("+waited[keys[i]]/waiting_for_ion*100+"%)");
 }
